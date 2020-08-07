cc8fa50eb326c33a236695d0e2bf950fd6388e4c
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-08-07 13:23:04 -0400

    Create setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,100 @@
+# Gits
+
+
+clear
+cd /opt/demo
+yum -y install git
+git clone https://github.com/tspannhw/meetup-sensors
+git clone https://github.com/tspannhw/FlinkSQLDemo
+git clone https://github.com/tspannhw/ApacheConAtHome2020
+
+chmod -R 777 /opt/demo/FlinkSQLDemo
+chmod -R 777 /opt/demo/meetup-sensors
+chmod -R 777 /opt/demo/ApacheConAtHome2020
+
+# No Kerberos 
+
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /user/admin
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /user/root
+HADOOP_USER_NAME=hdfs hdfs dfs -chown root:root /user/root
+HADOOP_USER_NAME=hdfs hdfs dfs -chown admin:admin /user/admin
+HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /user
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /user/kafka
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /tmp/sensors
+HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /tmp/sensors
+HADOOP_USER_NAME=hdfs hdfs dfs -chown kafka:kafka /user/kafka
+HADOOP_USER_NAME=hdfs hdfs dfs -chown kafka:kafka /tmp/sensors
+HADOOP_USER_NAME=hdfs hdfs dfs -mkdir /tmp/itemprice
+HADOOP_USER_NAME=hdfs hdfs dfs -chmod -R 777 /tmp/itemprice
+HADOOP_USER_NAME=hdfs hdfs dfs -chown kafka:kafka /tmp/itemprice
+
+impala-shell -i edge2ai-1.dim.local -d default -f sql/kudu.sql 
+
+curl -k -u admin:supersecret1 --location --request POST 'http://edge2ai-1.dim.local:31000/api/atlas/v2/types/typedefs' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "enumDefs": [],
+    "structDefs": [],
+    "classificationDefs": [],
+    "entityDefs": [
+        {
+            "name": "flink_application",
+            "superTypes": [
+                "Process"
+            ],
+            "serviceType": "flink",
+            "typeVersion": "1.0",
+            "attributeDefs": [
+                {
+                    "name": "id",
+                    "typeName": "string",
+                    "cardinality": "SINGLE",
+                    "isIndexable": true,
+                    "isOptional": false,
+                    "isUnique": true
+                },
+                {
+                    "name": "startTime",
+                    "typeName": "date",
+                    "cardinality": "SINGLE",
+                    "isIndexable": false,
+                    "isOptional": true,
+                    "isUnique": false
+                },
+                {
+                    "name": "endTime",
+                    "typeName": "date",
+                    "cardinality": "SINGLE",
+                    "isIndexable": false,
+                    "isOptional": true,
+                    "isUnique": false
+                },
+                {
+                    "name": "conf",
+                    "typeName": "map<string,string>",
+                    "cardinality": "SINGLE",
+                    "isIndexable": false,
+                    "isOptional": true,
+                    "isUnique": false
+                },
+                {
+                    "name": "inputs",
+                    "typeName": "array<string>",
+                    "cardinality": "LIST",
+                    "isIndexable": false,
+                    "isOptional": false,
+                    "isUnique": false
+                },
+                {
+                    "name": "outputs",
+                    "typeName": "array<string>",
+                    "cardinality": "LIST",
+                    "isIndexable": false,
+                    "isOptional": false,
+                    "isUnique": false
+                }
+            ]
+        }
+    ],
+    "relationshipDefs": []
+}'
