34618e14a06bed8a4561f3cd0736dec560c55e7c
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-08-24 10:23:32 -0400

    Update kudu.sql

--- sql/kudu.sql
+++ sql/kudu.sql
@@ -14,3 +14,115 @@ CREATE TABLE itemprice
 PRIMARY KEY (upc, originstore, updatedate) ) 
 PARTITION BY HASH PARTITIONS 4 
 STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+
+
+CREATE TABLE envirosensors 
+(
+uuid STRING, 
+	systemtime STRING,  
+	amplitude100 DOUBLE, 
+        amplitude500 DOUBLE, 
+	amplitude1000 DOUBLE, 
+	lownoise DOUBLE, 
+	midnoise DOUBLE,
+        highnoise DOUBLE, 
+	amps DOUBLE, 
+	ipaddress STRING, 
+	host STRING, 
+	host_name STRING,
+        macaddress STRING, 
+	endtime STRING, 
+	runtime STRING, 
+	starttime STRING, 
+        cpu DOUBLE, 
+	cpu_temp STRING, 
+	diskusage STRING, 
+	memory DOUBLE, 
+	id STRING, 
+	temperature STRING, 
+	adjtemp STRING, 
+	adjtempf STRING, 
+	temperaturef STRING, 
+	pressure DOUBLE, 
+	humidity DOUBLE, 
+	lux DOUBLE, 
+	proximity INT, 
+	oxidising DOUBLE, 
+	reducing DOUBLE, 
+	nh3 DOUBLE, 
+	gasko STRING,
+ PRIMARY KEY (uuid) ) 
+PARTITION BY HASH PARTITIONS 4 
+STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+
+
+CREATE TABLE energy 
+(
+uuid STRING, 
+	systemtime STRING,  
+        `current` INT, 
+	voltage INT, 
+	`power` INT, 
+	`total` INT, 
+	swver STRING, 
+	hwver STRING,
+	type STRING, 
+	model STRING, 
+	mac STRING, 
+	deviceId STRING, 
+	hwId STRING, 
+	fwId STRING, 
+	oemId STRING,
+	alias STRING, 
+	devname STRING, 
+	iconhash STRING, 
+	relaystate INT, 
+	ontime INT, 
+	activemode STRING, 
+	feature STRING, 
+	updating INT, 
+	rssi INT, 
+	ledoff INT, 
+	latitude INT, 
+	longitude INT, 
+	`day` INT, 
+	`index` INT, 
+	zonestr STRING, 
+	tzstr STRING, 
+	dstoffset INT, 
+	host STRING, 
+	currentconsumption INT, 
+	devicetime STRING, 
+	ledon STRING, 
+	fanstatus STRING, 
+	`end` STRING, 
+	te STRING, 
+	cpu INT, 
+	memory INT, 
+	diskusage STRING,
+ PRIMARY KEY (uuid) ) 
+PARTITION BY HASH PARTITIONS 4 
+STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+
+
+CREATE TABLE global_sensor_events 
+(uuid STRING, 
+ systemtime STRING, 
+ temperaturef STRING, 
+ pressure DOUBLE, 
+ humidity DOUBLE, 
+ lux DOUBLE, 
+ proximity INT, 
+ oxidising DOUBLE, 
+ reducing DOUBLE, 
+ nh3 DOUBLE, 
+ gasko STRING, 
+ `current` INT, 
+ voltage INT,
+ `power` INT, 
+ `total` INT, 
+ fanstatus STRING,
+  PRIMARY KEY (uuid) ) 
+PARTITION BY HASH PARTITIONS 4 
+STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+ 
