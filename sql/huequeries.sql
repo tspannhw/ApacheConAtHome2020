5d9551a261444248d616665d8288fa7d1a73fa61
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-08-31 13:59:57 -0400

    Update huequeries.sql

--- sql/huequeries.sql
+++ sql/huequeries.sql
@@ -1,3 +1,9 @@
 select version, setid, pubdate, title, link, guid, description
 from spl
-order by pubdate desc
+order by pubdate desc;
+
+
+select `location`, observation_time, latitude,longitude,temperature_string, weather, pressure_string
+from weatherkudu
+where `location` like '%NJ%'
+order by observation_time desc;
