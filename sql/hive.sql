10e522c73c290068844b4b759ec7711ceb4f469c
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-09-08 15:03:01 -0400

    Create hive.sql

---
+++ sql/hive.sql
@@ -0,0 +1,16 @@
+
+
+CREATE TABLE provenancehive
+(
+  eventId INT,
+  timestampMillis INT,
+  eventType STRING,
+  durationMillis INT,
+  lineageStart INT,
+  details STRING,
+  entityId STRING,
+  entityType STRING,
+  entitySize INT,
+  componentName STRING,
+   componentType STRING
+   )
