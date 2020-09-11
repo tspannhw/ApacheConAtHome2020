c10bfc9744bc0269ea5907eee8a83853e5a6ecca
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-09-11 14:05:23 -0400

    Update kudu.sql

--- sql/kudu.sql
+++ sql/kudu.sql
@@ -170,4 +170,18 @@ PRIMARY KEY (setID,version) )
 PARTITION BY HASH PARTITIONS 4 
 STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
 
+CREATE TABLE stocks
+(
+  uuid STRING,
+  `datetime` STRING,
+  `symbol` STRING, 
+  `open` STRING, 
+  `close` STRING,
+  `high` STRING,
+  `volume` STRING,
+  `low` STRING,
+PRIMARY KEY (uuid,`datetime`) ) 
+PARTITION BY HASH PARTITIONS 4 
+STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+
 
