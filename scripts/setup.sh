78e0ed559731b0a8f4794320428f2d3e2a5f5793
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-09-16 16:50:46 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -332,7 +332,7 @@ curl -X GET "http://ec2-54-167-28-79.compute-1.amazonaws.com:8585/api/v1/admin/m
 echo ""
 echo ""
 echo "▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔"
-echo "Build Alerts"
+echo "Build Kafka Alerts"
 echo "▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔"
 echo ""
 echo ""
@@ -341,11 +341,7 @@ curl -X GET "http://edge2ai-1.dim.local:9991/api/v1/admin/alert/notifications?li
 
 # POST to build A Notifier
 
-curl -X POST "http://edge2ai-1.dim.local:9991/api/v1/admin/notifiers" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"type\": \"avro\", \"schemaGroup\": \"Kafka\", \"name\": \"$schemaname\", \"description\": \"schemaname\", \"compatibility\": \"BOTH\", \"validationLevel\": \"LATEST\"}"
-
-{"id":null,"name":"http","description":"http","notifierProviderId":"http",
-"rateLimiterConfig":{"count":1,"duration":"MINUTE"},
-"config":{"URL":"http://edge2ai-1.dim.local:9999/alerts","ConnectionTimeoutInMilliSecs":30000,"ReadTimeoutInMilliSecs":30000}}
+curl -X POST "http://edge2ai-1.dim.local:9991/api/v1/admin/notifiers" -H "accept: application/json" -H "Content-Type: application/json" -d '{"id":null,"name":"http","description":"http","notifierProviderId":"http","rateLimiterConfig":{"count":1,"duration":"MINUTE"},"config":{"URL":"http://edge2ai-1.dim.local:9999/alerts","ConnectionTimeoutInMilliSecs":30000,"ReadTimeoutInMilliSecs":30000}}'
 
 echo ""
 
@@ -357,12 +353,20 @@ echo ""
 # POST to Add a new alert Policy
 for f in /opt/demo/ApacheConAtHome2020/alerts/*.json
 do 
-  curl -X POST "http://edge2ai-1.dim.local:8585/api/v1/admin/kalertPolicy" -H "accept: application/json" -H "Content-Type: application/json" -d @$f
+  echo "Load alert $f"
+  curl -X POST "http://edge2ai-1.dim.local:8585/api/v1/admin/alertPolicy" -H "accept: application/json" -H "Content-Type: application/json" -d @$f
   echo ""
 done
 
+
 # GET the list of alert policies created
 curl -X GET "http://edge2ai-1.dim.local:9991/api/v1/admin/alertPolicy"
+
+# To Disable all policies, iterate all policies, go by id
+# http://edge2ai-1.dim.local:9991/api/v1/admin/alertPolicy/45/disable
+
+# To DELETE all policies, iterate all policies, go by id
+# http://edge2ai-1.dim.local:9991/api/v1/admin/alertPolicy/6
 
 echo ""
 echo ""
