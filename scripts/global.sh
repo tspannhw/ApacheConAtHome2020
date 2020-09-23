7a1289ec6ef207e84a5337823182073441c446b4
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-09-23 14:43:18 -0400

    Create global.sh

---
+++ scripts/global.sh
@@ -0,0 +1,7 @@
+flink-sql-client embedded -e /opt/demo/ApacheConAtHome2020/conf/sql-env-insert.yaml -u "
+INSERT INTO global_sensor_events 
+SELECT 
+	scada.uuid, 	scada.systemtime ,  scada.temperaturef , scada.pressure , scada.humidity , scada.lux , 
+scada.proximity , scada.oxidising ,scada.reducing , scada.nh3 , scada.gasko,energy.`current`, energy.voltage ,energy.`power` ,energy.`total`,energy.fanstatus
+FROM energy,scada
+WHERE scada.systemtime = energy.systemtime;"
