1d399f13030be50f77663a986660e7e87f49959f
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-09-28 14:45:34 -0400

    Update hivetables.sql

--- hive/hivetables.sql
+++ hive/hivetables.sql
@@ -9,3 +9,34 @@ LOCATION '/iextradingquote';
 CREATE EXTERNAL TABLE IF NOT EXISTS iextradingnews (`datetime` STRING, headline STRING, source STRING, url STRING, summary STRING, related STRING, image STRING) 
 STORED AS ORC 
 LOCATION '/iextradingnews';
+
+CREATE TABLE `twitterhive` (
+  `msg` STRING,
+  `unixtime` STRING,
+  `friends_count` STRING,
+  `hashtags` STRING,
+  `listed_count` STRING,
+  `tweet_id` STRING,
+  `user_name` STRING,
+  `favourites_count` STRING,
+  `source` STRING,
+  `placename` STRING,
+  `media_url` STRING,
+  `retweet_count` STRING,
+  `user_mentions_name` STRING,
+  `geo` STRING,
+  `urls` STRING,
+  `countrycode` STRING,
+  `user_url` STRING,
+  `place` STRING,
+  `coordinates` STRING,
+  `handle` STRING,
+  `profile_image_url` STRING,
+  `time_zone` STRING,
+  `ext_media` STRING,
+  `statuses_count` STRING,
+  `followers_count` STRING,
+  `location` STRING,
+  `user_mentions` STRING,
+  `user_description` STRING
+);
