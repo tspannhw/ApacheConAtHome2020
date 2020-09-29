168d5452ccd9f61854b37e5ae562aac704eb35e4
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-09-29 15:06:32 -0400

    Update stocksquery.sql

--- sql/stocksquery.sql
+++ sql/stocksquery.sql
@@ -1,2 +1,7 @@
 select * from stocks
-order by `datetime` desc
+order by `datetime` desc;
+
+
+select `datetime`, cast(`high` as float) highStockValue, `symbol`,  cast(`low` as float) lowStockValue, 
+ cast(`open` as float) openingStockValue,  cast(`close` as float) closingStockValue
+from stocks
