65b80c4c0a9b09ce0aa59e6898188f36c5cf8c1a
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-10-13 11:42:02 -0400

    Create setupnifi.sh

---
+++ scripts/setupnifi.sh
@@ -0,0 +1,44 @@
+  
+clear
+
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
+echo "┃ Starting to load NiFi Paramters   ┃"
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
+echo " *** Thanks Paul Vidal *** "
+echo " *** Thanks Andre *** "
+echo " *** Thanks Dan ***"
+echo ""
+echo "▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔"
+echo "⏱  $(date +%H%Mhrs)"
+echo "▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔"
+echo ""
+echo ""
+
+# yum -y install git curl wget gunzip gzip tar
+
+# /opt/demo/nifi-toolkit-1.12.0/bin/cli.sh nifi import-param-context -u http://localhost:8080 -i retail.json
+# https://nifi.apache.org/docs/nifi-docs/html/toolkit-guide.html
+
+echo "Download NiFi Toolkit"
+
+# Download NiFi Toolkit
+#
+# cd /opt/demo
+# wget https://apache.claz.org/nifi/1.12.1/nifi-toolkit-1.12.1-bin.tar.gz
+# gunzip nifi-toolkit-1.12.1-bin.tar.gz
+# tar -xvf nifi-toolkit-1.12.1-bin.tar
+
+echo "Load Parameter Contexts"
+
+for f in /opt/demo/ApacheConAtHome2020/parameters/*.json
+do 
+
+echo "Uploading Parameter File $f"
+
+/opt/demo/nifi-toolkit-1.12.0/bin/cli.sh nifi import-param-context -u http://localhost:8080 -i $f
+
+echo "Added."
+echo ""
+
+done
+
