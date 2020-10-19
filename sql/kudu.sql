59324663e42202798e7a48ec8ff38f016773321a
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-10-19 17:24:21 -0400

    Update kudu.sql

--- sql/kudu.sql
+++ sql/kudu.sql
@@ -201,3 +201,20 @@ PARTITION BY HASH PARTITIONS 4
 STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
 
 
+
+CREATE TABLE IF NOT EXISTS sentiment_tweets 
+(tweet_id STRING, `unixtime` STRING, friends_count STRING, hashtags STRING, listed_count STRING, 
+source STRING, favourites_count STRING, 
+statuses_count STRING, followers_count STRING, `location` STRING, msg STRING, time STRING, sentiment STRING, 
+user_name STRING, placename STRING, retweet_count STRING, 
+user_mentions_name STRING, geo STRING, urls STRING, countryCode STRING, user_url STRING,
+place STRING, coordinates STRING, handle STRING, profile_image_url STRING, time_zone STRING,
+ ext_media STRING, user_description STRING,  `media_url` STRING,`timestamp` STRING, user_mentions STRING,
+PRIMARY KEY (tweet_id, `unixtime`)
+)
+PARTITION BY HASH PARTITIONS 4
+STORED AS KUDU
+TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+
+
+
