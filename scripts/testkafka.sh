ff3f0a9ff779237558fc1fa688415f9a95975016
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-10-20 08:12:59 -0400

    Update testkafka.sh

--- scripts/testkafka.sh
+++ scripts/testkafka.sh
@@ -30,3 +30,48 @@ INSERT INTO WindowedQuantity
 SELECT TUMBLE_START(event_time, INTERVAL '10' SECOND) as window_start, itemId, sum(quantity) as volume
 FROM ItemTransactions
 GROUP BY itemId, TUMBLE(event_time, INTERVAL '10' SECOND);
+
+CREATE TABLE Queries (
+	queryId    BIGINT,
+	ts    BIGINT,
+	itemId    STRING,
+	event_time AS CAST(from_unixtime(floor(ts/1000)) AS TIMESTAMP(3)),
+	WATERMARK FOR event_time AS event_time - INTERVAL '5' SECOND
+) WITH (
+	'connector.type'    	 = 'kafka',
+	'connector.version' 	 = 'universal',
+	'connector.topic'   	 = 'query.input.log.1',
+	'connector.startup-mode' = 'earliest-offset',
+	'connector.properties.bootstrap.servers' = 'edge2ai-1.dim.local:9092',
+	'format.type' = 'json'
+);
+
+CREATE TABLE QueryResult (
+  queryId    BIGINT,
+	query_time    TIMESTAMP(3),
+	itemId    STRING,
+	quantity INT
+) WITH (
+	'connector.type'    	 = 'kafka',
+	'connector.version' 	 = 'universal',
+	'connector.topic'   	 = 'query.output.log',
+	'connector.properties.bootstrap.servers' = 'edge2ai-1.dim.local:9092',
+	'format.type' = 'json'
+);
+
+INSERT INTO QueryResult
+SELECT
+  q.queryId,
+  TUMBLE_START(q.event_time, INTERVAL '1' SECOND) as query_time,
+  t.itemId,
+  sum(t.quantity) AS quantity
+FROM
+  ItemTransactions AS t,
+  Queries AS q
+WHERE
+  t.itemId = q.itemId AND
+  t.event_time BETWEEN q.event_time - INTERVAL '1' MINUTE AND q.event_time
+GROUP BY
+  t.itemId, q.queryId, TUMBLE(q.event_time, INTERVAL '1' SECOND);
+  
+  
