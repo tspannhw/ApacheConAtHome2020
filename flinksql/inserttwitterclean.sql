8f9527d66164c3306d380a105d37c5f5c3d00aa7
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-10-23 14:21:51 -0400

    Update inserttwitterclean.sql

--- flinksql/inserttwitterclean.sql
+++ flinksql/inserttwitterclean.sql
@@ -2,4 +2,4 @@ INSERT INTO globaltwitter
 SELECT friends_count, tweet_id, source, `time` as systemtime, location, sentiment
 FROM twitter
 WHERE
-    sentiment is not null;
+    sentiment is not null and sentiment <> 'null' and trim(sentiment) <> '';
