da9d0cb954da105829b03809338e34ff777da484
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-10-27 15:05:16 -0400

    Create insert3.sql

---
+++ flinksql/insert3.sql
@@ -0,0 +1,26 @@
+
+
+
+INSERT INTO global_sensor_events 
+SELECT 
+	scada.uuid, 
+	scada.systemtime ,  
+scada.temperaturef , 
+scada.pressure , 
+scada.humidity , 
+scada.lux , 
+scada.proximity , 
+scada.oxidising , 
+scada.reducing , 
+scada.nh3 , 
+scada.gasko,
+energy.`current`, 
+energy.voltage ,
+energy.`power` ,
+energy.`total`,
+energy.fanstatus
+
+FROM energy,
+     scada
+WHERE
+    scada.systemtime >= energy.systemtime;
