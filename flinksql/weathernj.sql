e882a5ad5854a8478e915b3430c7106e2869ca64
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-10-29 11:30:16 -0400

    Create weathernj.sql

---
+++ flinksql/weathernj.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO weathernj 
+SELECT `location`, station_id,latitude,longitude,observation_time,weather,
+temperature_string, temp_f,temp_c,relative_humidity,wind_string,wind_dir,wind_degrees,wind_mph,
+wind_kt, pressure_in,dewpoint_string,dewpoint_f,dewpoint_c
+FROM weather
+WHERE
+    `location` is not null and `location` <> 'null' and trim(`location`) <> '';
