c50c63b0579fde7ad902c416dc3be68e8b45408c
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-11-11 16:42:09 -0500

    Update kudu.sql

--- sql/kudu.sql
+++ sql/kudu.sql
@@ -218,3 +218,16 @@ TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
 
 
 
+CREATE TABLE webrequests
+(
+  uuid STRING,
+  msgdate TIMESTAMP,
+  controllerid STRING, 
+  endpointid STRING, 
+  remoteaddress STRING,
+  sessionid STRING,
+  message STRING,
+  localaddress STRING,
+PRIMARY KEY (uuid,msgdate) ) 
+PARTITION BY HASH PARTITIONS 4 
+STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
