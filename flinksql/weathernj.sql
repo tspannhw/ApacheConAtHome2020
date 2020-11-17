4db0ec9ad0d62e7c037e0039c085c1dad4adb962
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-11-17 12:01:28 -0500

    Update weathernj.sql

--- flinksql/weathernj.sql
+++ flinksql/weathernj.sql
@@ -1,8 +1,24 @@
 
 INSERT INTO weathernj 
-SELECT `location`, station_id,latitude,longitude,observation_time,weather,
-temperature_string, temp_f,temp_c,relative_humidity,wind_string,wind_dir,wind_degrees,wind_mph,
-wind_kt, pressure_in,dewpoint_string,dewpoint_f,dewpoint_c
+SELECT CAST(`location` as STRING) `location`, 
+CAST(station_id as STRING) station_id,
+     latitude,
+     longitude,
+CAST(observation_time as STRING) observation_time,
+CAST(weather as STRING) weather,
+CAST(temperature_string as STRING) temperature_string, 
+     temp_f,
+     temp_c,
+     relative_humidity,
+CAST(wind_string  as STRING) wind_string,
+CAST(wind_dir as STRING) wind_dir,
+     wind_degrees,
+     wind_mph,
+     wind_kt, 
+     pressure_in,
+CAST(dewpoint_string as STRING) dewpoint_string,
+     dewpoint_f,
+     dewpoint_c
 FROM weather
 WHERE
     `location` is not null and `location` <> 'null' and trim(`location`) <> '' and `location` like '%NJ';
