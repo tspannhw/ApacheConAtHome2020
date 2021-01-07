bb3697ae293ab011e69be23ff0bcabfac0f555fe
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-01-07 15:25:04 -0500

    Update kudu.sql

--- sql/kudu.sql
+++ sql/kudu.sql
@@ -15,6 +15,20 @@ TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
 
 	
 
+CREATE TABLE transcomevents ( 
+uuid STRING, `ts` TIMESTAMP,  
+`title` STRING, `description` STRING, 
+`pubDate` STRING, 
+`point` STRING,
+PRIMARY KEY (uuid)
+)
+PARTITION BY HASH PARTITIONS 4
+STORED AS KUDU
+TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+
+
+   
+   
 CREATE TABLE itemprice
 (
   upc STRING,
