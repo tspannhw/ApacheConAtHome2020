879aafa276855a3fda3dcb19eb28c5c1f9cd9a53
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-03-12 16:45:10 -0500

    Update kudu.sql

--- sql/kudu.sql
+++ sql/kudu.sql
@@ -16,13 +16,14 @@ TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
 	
 
 CREATE TABLE clouderastatus ( 
-   uuid STRING,`ts` TIMESTAMP,  
-  title STRING, 
-  pubdate STRING, 
-  link STRING,
-  description STRING,
-  guid STRING,
-PRIMARY KEY (uuid,ts ) )
+   `uuid` STRING,
+	`ts` TIMESTAMP,  
+`title` STRING, 
+	`description` STRING, 
+`pubdate` STRING, 
+  `link` STRING,
+  `guid` STRING,
+PRIMARY KEY (`uuid`,`ts` ) )
 PARTITION BY HASH PARTITIONS 4 
 STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
 
