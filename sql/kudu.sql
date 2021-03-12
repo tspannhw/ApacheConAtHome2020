b5eff7b9cdf926d886ddc2ce30947e06c851272e
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-03-12 16:34:20 -0500

    Update kudu.sql

--- sql/kudu.sql
+++ sql/kudu.sql
@@ -15,6 +15,17 @@ TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
 
 	
 
+CREATE TABLE clouderastatus ( 
+   uuid STRING,`ts` TIMESTAMP,  
+  title STRING, 
+  pubdate STRING, 
+  link STRING,
+  description STRING,
+  guid STRING,
+PRIMARY KEY (uuid,ts ) 
+PARTITION BY HASH PARTITIONS 4 
+STORED AS KUDU TBLPROPERTIES ('kudu.num_tablet_replicas' = '1');
+
 CREATE TABLE transcomevents ( 
 uuid STRING, `ts` TIMESTAMP,  
 `title` STRING, `description` STRING, 
